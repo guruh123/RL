c7187c613bedbfa6f18a34ab1450ede59e12b32a
Chila29 <86872622+Chila29@users.noreply.github.com>
2021-10-13 18:03:36 +0800

    Delete faq.php

--- faq.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $name = "faq";
-    include('components/head.inc.php');
-    include('layouts/navbarback.inc.php');
-    include('layouts/faq.inc.php');
-    include('components/footer.inc.php');
-?>
