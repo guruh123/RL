2c0eb302e0508299d347817172db4e226f73c22a
Chila29 <86872622+Chila29@users.noreply.github.com>
2021-10-13 18:04:05 +0800

    Delete login.php

--- login.php
+++
@@ -1,8 +0,0 @@
-<?php
- $name = "login";
- include('components/connectdb.inc.php');
- include('components/head.inc.php'); 
- include('layouts/navbarback.inc.php'); 
- include('layouts/loginres.inc.php'); 
- include('components/footer.inc.php');
-?>
